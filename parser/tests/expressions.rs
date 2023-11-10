bedb2b56f2887507344491bf53a3058dca80a1bc
Ben <kaleidawave@gmail.com>
2023-11-10 11:26:38 +0000

    Small fixes (#79)

--- parser/tests/expressions.rs
+++ parser/tests/expressions.rs
@@ -25,3 +25,25 @@ param => {
 	// let output = module.to_string(&ToStringOptions::typescript());
 	// assert_eq!(output, input);
 }
+
+#[test]
+fn function_calls() {
+	let input = r#"
+x(4, 5);
+y.t(2, 3);
+y.t<4, 2>(3);
+y.t<4, Array<5>>(3);
+a(y<2>(4));
+a.a?.(y<2>(4));
+a.a(...expr, y)
+    "#
+	.trim();
+
+	let module =
+		Module::from_string(input.to_owned(), Default::default(), SourceId::NULL, None).unwrap();
+
+	eprintln!("Module: {:#?}", module);
+
+	let output = module.to_string(&ezno_parser::ToStringOptions::typescript());
+	assert_eq!(output, input);
+}
