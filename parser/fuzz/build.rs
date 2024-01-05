daef4f444e59a888980a489eaf29bf9d684ae2d4
Ben <kaleidawave@gmail.com>
2024-01-05 17:57:23 +0000

    Minor fixes

--- parser/fuzz/build.rs
+++ parser/fuzz/build.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 	let out_path = Path::new(&std::env::var("OUT_DIR")?).join("common.rs");
 	let mut out = File::create(out_path)?;
 	reqwest::blocking::get(
-		"https://raw.githubusercontent.com/boa-dev/boa/main/fuzz/fuzz_targets/common.rs",
+		"https://raw.githubusercontent.com/boa-dev/boa/main/tests/fuzz/fuzz_targets/common.rs",
 	)?
 	.copy_to(&mut out)?;
 
