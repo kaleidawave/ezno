73e55b7d87a196008a9db749834fb5b3e556887f
Ben <kaleidawave@gmail.com>
2026-06-16 20:48:38 +0100

    - Update description - Update blog URL - Remove JS based plugin - Remove references to sponsor funding

--- src/js-based-plugin/types.d.ts
+++
@@ -1,8 +0,0 @@
-export type ReadFromFS = (path: string) => string | null;
-
-export interface EznoUnpluginOptions {
-    /** Defaults to only running on .ezno.* files */
-    all_js_ts_files?: boolean,
-
-    customBuild?: (cb: ReadFromFS, entryPath: string, minify: boolean) => any
-}
